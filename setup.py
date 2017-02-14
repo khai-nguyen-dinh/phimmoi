55e8d3cca80fa700762d143f530eaea5c85e6bdb
khainguyen <khainguyenptiter@gmail.com>
2017-02-14 21:22:02 +0700

    Change docker-compose file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='crawl_film',
+    version='',
+    packages=['crawl_film.crawl_film', 'crawl_film.crawl_film.spiders', 'webservice.website',
+              'webservice.website.migrations', 'webservice.webservice'],
+    url='',
+    license='',
+    author='khainguyen',
+    author_email='',
+    description=''
+)
